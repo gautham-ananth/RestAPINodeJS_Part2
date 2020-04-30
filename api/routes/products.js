d46073a1abcff27398be2b8a4bc0c3143368245d
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-04-30 13:16:40 +0530

    CRUD done for products routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//GET All Products
 router.get('/', (req, res, next) => {
     res.status(200).json(
         {
@@ -8,11 +9,51 @@ router.get('/', (req, res, next) => {
         })
 })
 
+//Post a Product
 router.post('/', (req, res, next) => {
     res.status(200).json(
         {
             message: "Handling POST Requests done"
         })
+})
+
+//Get one Product
+router.get('/:productID', (req, res, next) => {
+    const productID = req.params.productID;
+    console.log("productID :", productID)
+    if (productID === 'special') {
+        {
+            res.status(200).json(
+                {
+                    message: "You discoverd the special id",
+                    id: productID
+                })
+        }
+    }
+    else {
+        res.status(404).json(
+            {
+                message: "You discoverd the wrong id",
+                id: productID
+            })
+
+    }
+
+})
+
+//Patch or Update one Product
+router.patch('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: "Updated the Product : " + req.params.productID
+    })
+})
+
+
+//Delete one Product
+router.delete('/:productID', (req, res, next) => {
+    res.status(200).json({
+        message: "Delete the Product : " + req.params.productID
+    })
 })
 
 

23395b5bbf982c767f6736ba384a9f0358a10aac
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-05-02 23:23:11 +0530

    email+pwd validation done using bcrypt npm

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -43,6 +44,7 @@ app.use((req, res, next) => {
 //Middleware routes to handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 //handle other requests
 app.use((req, res, next) => {
